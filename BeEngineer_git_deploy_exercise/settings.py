6fbb42a7577bcd892beb67c6b575e78ffa96f1b7
山口結愛 <yamaguchiyui@yamaguchiyuanoMacBook-Air.local>
2022-12-18 17:38:41 +0900

    second

--- BeEngineer_git_deploy_exercise/settings.py
+++ BeEngineer_git_deploy_exercise/settings.py
@@ -112,3 +112,13 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+# ...
+try:
+    from .local_settings import *
+except ImportError:
+    # local_settings.py が存在しなくてもエラーにならないようにする
+    pass
